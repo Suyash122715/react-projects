bcc9b378863db3b55518d4c26ba946f41b3e6ef2
Suyash Barnawal <24ug1bycs692@bmsit.in>
2026-08-09 16:27:21 +0530

    Context api learning

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1> This is project for context api</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from 'react';
+import UserContext from '../context/UserContext';
+
+function Login(){
+    const [username, setUsername]=useState('')
+    const[password, setPassword]=useState('')
+    const {setUser} = useContext(UserContext)
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setUser({username, password})
+    }
+    return(
+        <div>
+            <h2>Login</h2>
+            <input 
+            onChange={(e)=>setUsername(e.target.value)}
+            type='text' 
+            value={username}
+            placeholder='username'/>
+            {"      "}
+            <input 
+            onChange={(e)=>setPassword(e.target.value)}
+            type='password' 
+            value={password}
+            placeholder='password'/>
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
